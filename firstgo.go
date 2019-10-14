81260a7b3c9ff0cebf89d12ced639c15d44b2524
Beta-23 <alfred.dominguez@gmail.com>
2019-10-14 15:49:50 -0700

    Set up var to add two float numbers and print sum

--- firstgo.go
+++ firstgo.go
@@ -20,6 +20,10 @@ func main() {
 	cal()
 	numgen()
 	
+	var num1 float64 = 8.5
+	var num2 float64 = 6.5
+	fmt.Println("4. The sum of", num1, "and", num2, "is")
+	fmt.Println(add(num1,num2))
 }
 
 // This will execute via the main function
@@ -28,11 +32,15 @@ func cal() {
 	fmt.Println ("2. The square root of 36 is",math.Sqrt(36))
 }
 
-// This will generate a number between 0-99
+// This will generate a non-static number between 0-99
 func numgen() {
 	rand.Seed(time.Now().UnixNano())
 	fmt.Println ("3. A number between 0 and 99!",rand.Intn(100))
 	fmt.Println()
+}
+
+func add(x float64, y float64) float64 {
+	return x+y
 }
 
 
